5003a2e620dcd0d5450d38a3cd750b25780db725
Pranav Agnihotri <pranavagnihotri143@gmail.com>
2021-10-15 23:39:22 -0400

    create login page without backend user auth

--- ui/src/mainPage/home.js
+++ ui/src/mainPage/home.js
@@ -1,14 +1,19 @@
 import * as React from "react";
 import { Link, Router } from "@reach/router";
 import { SearchAndCourse } from "../components/SearchWithList";
-
-const Login = () => (
-  <div>
-    <h2>Login</h2>
-  </div>
-);
+import { Login } from "../components/Login"
+import { useState, useEffect } from "react"
 
 function Home() {
+  // To-Do: Write code to authenticate user. While authToken doesn't exist, we should show login page
+  const [authToken, setAuthToken] = useState('')
+
+
+  // To-Do: Implement credential checking in backend for Login component
+  const checkCredentials = (loginInfo) => {
+    console.log('CHECKING INFO', loginInfo)
+  }
+
   return (
     <>
       <nav>
@@ -18,7 +23,7 @@ function Home() {
       </nav>
       <Router>
         <SearchAndCourse path="/" />
-        <Login path="/login" />
+        <Login path="/login" onFormSubmit={checkCredentials} />
       </Router>
     </>
   );
