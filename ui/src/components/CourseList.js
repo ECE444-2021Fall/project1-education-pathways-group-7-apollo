63f029b1ea3818a719c2207b4c6c3a5088e29a25
TobyKurniawan <38872576+TobyKurniawan@users.noreply.github.com>
2021-10-25 16:27:06 -0400

    Course Descriptions draft without API  (#2)
    * Used material UI table for pagination, and dropdown expansion
    
    * Changed template for data displayed in course description
    
    * merge with main
    
    * Finished functional UI for course descriptions, styling finalization will be done later, must link to API now

--- ui/src/components/CourseList.js
+++ ui/src/components/CourseList.js
@@ -1,51 +1,281 @@
+import Collapse from "@mui/material/Collapse";
+import TableHead from "@mui/material/TableHead";
+import Typography from "@mui/material/Typography";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import * as React from "react";
+import PropTypes from "prop-types";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
+import TableFooter from "@mui/material/TableFooter";
+import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import AddIcon from "@mui/icons-material/Add";
+import LastPageIcon from "@mui/icons-material/LastPage";
+import { styled } from "@mui/material/styles";
 
-function createData(code, title, desc) {
-  return { code, title, desc };
-}
+const TableStyle = styled("div")({
+  width: "50%",
+});
 
-const rows = [
-  createData("ECE444", "Software Engineering", "This course lol"),
-  createData("ECE568", "Computer Security", "foo"),
-  createData("APS100", "Orientation to Engineering", "bar"),
-  createData("CIV300", "Terrestrial Energy Systems", "foobar"),
-];
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
 
-const CourseList = (props) => {
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Course Code</TableCell>
-            <TableCell align="right">Title</TableCell>
-            <TableCell align="right">&nbsp;Description</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.code}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.code}
-              </TableCell>
-              <TableCell align="right">{row.title}</TableCell>
-              <TableCell align="right">{row.desc}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
   );
 }
 
-export {CourseList};
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
+function createData(code, title, fac, desc, avg, offerings) {
+  return {
+    code,
+    title,
+    fac,
+    desc,
+    avg,
+    offerings,
+  };
+}
+
+function Row(props) {
+  const {
+    row,
+    addedCourses,
+    setAddedCourses,
+    addedCoursesMap,
+    setAddedCoursesMap,
+  } = props;
+  const [open, setOpen] = React.useState(false);
+
+  const addCourses = React.useCallback(
+    (row) => {
+      if (!addedCoursesMap.hasOwnProperty(row)) {
+        const newCourses = { ...addedCoursesMap, [row]: "Dummy data" };
+        setAddedCoursesMap(newCourses);
+        const newCourseArray = [...addedCourses, row];
+        setAddedCourses(newCourseArray);
+      }
+    },
+    [addedCourses, setAddedCourses, addedCoursesMap, setAddedCoursesMap]
+  );
+
+  return (
+    <React.Fragment>
+      <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.code}
+        </TableCell>
+        <TableCell align="left">{row.title}</TableCell>
+        <TableCell align="left">{row.fac}</TableCell>
+        <TableCell>
+          <IconButton
+            aria-label="add-course"
+            size="small"
+            onClick={() => addCourses(row.code)}
+          >
+            <AddIcon />
+          </IconButton>
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Typography
+                aria-label="course-desc"
+                variant="h6"
+                gutterBottom
+                component="div"
+              >
+                Description
+              </Typography>
+              <div>{row.desc}</div>
+              <Table size="small" aria-label="advanced course info">
+                <TableRow>
+                  <TableCell>Course Average</TableCell>
+                  <TableCell>Offerings</TableCell>
+                </TableRow>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>{row.avg}</TableCell>
+                    <TableCell>{row.offerings}</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+
+const rows = [
+  createData(
+    "ECE444",
+    "Software Engineering",
+    "Applied Science and Engineering",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "B",
+    ["Fall 2022, Winter 2022"]
+  ),
+  createData("ECE100", "Orientation to ECE", "Applied Science and Engineering"),
+  createData("ECE496", "Capstone", "Applied Science and Engineering"),
+];
+
+const CourseList = (props) => {
+  const { setAddedCourses, addedCourses, addedCoursesMap, setAddedCoursesMap } =
+    props;
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  return (
+    <TableStyle>
+      <TableContainer component={Paper}>
+        <Table aria-label="collapsible table">
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <TableCell align="left">Course Code</TableCell>
+              <TableCell align="left">Title</TableCell>
+              <TableCell align="left">Faculty</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {(rowsPerPage > 0
+              ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : rows
+            ).map((row) => (
+              <Row
+                key={row.name}
+                row={row}
+                addedCourses={addedCourses}
+                setAddedCourses={setAddedCourses}
+                addedCoursesMap={addedCoursesMap}
+                setAddedCoursesMap={setAddedCoursesMap}
+              />
+            ))}
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                colSpan={3}
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: {
+                    "aria-label": "rows per page",
+                  },
+                  native: true,
+                }}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    </TableStyle>
+  );
+};
+
+export { CourseList };
